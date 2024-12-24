aff046d3e38ef7e96ffd99feb80f9d5ff27bf06c
Reysl <rasulnasaraliev@gmail.com>
2024-12-24 19:18:30 +0300

    Initial commit

---
+++ app/src/main/java/com/reysl/diaryapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.reysl.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.reysl.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiaryAppTheme {
+
+            }
+        }
+    }
+}
